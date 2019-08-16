3400af0c8a8b8456936df24efc115d0bdcea2c96
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-08-16 14:32:27 -0300

    Moves store from Game to Board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,36 +1,49 @@
 import React from 'react'
 import './index.css'
+import { connect } from 'react-redux'
+import { tickSquare } from '../../actions'
 import SquareButton from '../Square/SquareButton'
 import SquareInfo from '../Square/SquareInfo'
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <SquareButton
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    )
+const mapStateToProps = ({ history, stepNumber }) => ({
+  history, stepNumber
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  handleClick: index => {
+    dispatch(tickSquare(index))
   }
+})
 
-  render() {
-    let i = 0
+const Board = (props) => {
+  const history = props.history
+  const current = history[props.stepNumber]
+  let i = 0
 
-    return (
-      <>
+  const renderSquareButton = (i) => (
+    <SquareButton
+      value={current.squares[i]}
+      onClick={() => props.handleClick(i)}
+    />
+  )
+
+  return (
+    <>
+      <div className="board-row">
+        { [0, 1, 2].map((col) => <SquareInfo value={col} />) }
+      </div>
+
+      { [0, 1, 2].map((row) => 
         <div className="board-row">
-          { [0, 1, 2].map((col) => <SquareInfo value={col} />) }
+          { [0, 1, 2].map((col) => renderSquareButton(i++)) }
+          { <SquareInfo value={row} />}
         </div>
-
-        { [0, 1, 2].map((row) => 
-          <div className="board-row">
-            { [0, 1, 2].map((col) => this.renderSquare(i++)) }
-            { <SquareInfo value={row} />}
-          </div>
-        ) }
-      </>
-    )
-  }
+      ) }
+    </>
+  )
 }
 
-export default Board
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Board)
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,36 +1,15 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { tickSquare } from '../../actions'
 import './index.css'
 import Board from '../Board'
 
-const mapStateToProps = ({ history, stepNumber }) => ({
-  history, stepNumber
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  handleClick: index => {
-    dispatch(tickSquare(index))
-  }
-})
-
 const Game = (props) => {
-  const history = props.history
-  const current = history[props.stepNumber]
-
   return (
     <div className="game">
       <div className="game-board">
-        <Board
-          squares={current.squares}
-          onClick={(i) => props.handleClick(i)}
-        />
+        <Board />
       </div>
     </div>
   )
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Game)
+export default Game

3cd3ddd4ce712f6e737201d73484b89e00742faa
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-08-18 19:16:26 -0300

    Add tests to App render

---
+++ src/__tests__/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from '../reducers'
+import '../index.css'
+import Game from '../components/Game'
+
+const store = createStore(rootReducer)
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <Game />
+    </Provider>,
+    div
+  )
+})

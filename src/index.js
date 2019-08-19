caae96dda37c248d982254f604610ba8877a424c
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-08-19 15:22:35 -0300

    Add logger through middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import './index.css'
 import Game from './components/Game'
 
-const store = createStore(rootReducer)
+const logger = createLogger()
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(
   <Provider store={store}>

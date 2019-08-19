10d341dc611704ce1e7c3b5d7995ee9854427ea8
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-08-19 15:23:05 -0300

    Group action types and action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,19 @@
 export const TICK_SQUARE = 'TICK_SQUARE'
-export const UPDATE_BOARD = 'UPDATE_BOARD'
-export const ADD_HISTORY = 'ADD_HISTORY'
-export const JUMP_TO = 'JUMP_TO'
-
 export const tickSquare = (position) => ({
   type: TICK_SQUARE, position
 })
 
+export const UPDATE_BOARD = 'UPDATE_BOARD'
 export const updateBoard = (board) => ({
   type: UPDATE_BOARD, board
 })
 
+export const ADD_HISTORY = 'ADD_HISTORY'
 export const addHistory = (position) => ({
   type: ADD_HISTORY, position
 })
 
+export const JUMP_TO = 'JUMP_TO'
 export const jumpTo = (step) => ({
   type: JUMP_TO, step
 })

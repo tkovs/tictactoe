94b72a198c124f285bedc5d26ff185c415ac5865
tkovs <joao.rodrigues@arapiraca.ufal.br>
2019-08-20 12:42:20 -0300

    Refactor reducer to use const variables

--- src/redux/reducers/squares.js
+++ src/redux/reducers/squares.js
@@ -3,13 +3,14 @@ import * as types from '../../actions/types'
 export default function reducer(state = Array(9).fill(null), action) {
   switch(action.type) {
     case types.TICK_SQUARE:
-      let squares = state.slice()
-
-      const nulls = squares.filter(s => s === null).length
+      const nulls = state.filter(s => s === null).length
       const player = (nulls % 2 === 0) ? 'O' : 'X'
-      squares[action.payload.position] = player
 
-      return squares
+      return [
+        ...state.slice(0, action.payload.position),
+        player,
+        ...state.slice(action.payload.position + 1)
+      ]
     default:
       return state
   }
